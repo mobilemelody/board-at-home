50e406b1c219671f3190ae99c4c320a11d3c8692
Jed Piezas <jed.piezas@gmail.com>
2020-07-03 20:32:57 -0700

    Add boilerplate to Dockerize app

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.status(200).send({message: 'test'});
+  res.status(200).send({message: 'suhh dude'});
 });
 
 module.exports = router;
