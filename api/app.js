94388811264b4923c2832b50ddf270e31269c1f1
Jed Piezas <jed.piezas@gmail.com>
2020-07-03 19:22:02 -0700

    Connect the front end and back end together

--- api/app.js
+++ api/app.js
@@ -5,9 +5,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
