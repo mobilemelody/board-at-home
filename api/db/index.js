92e26824e6d44751492b1aa2842f873d8d1355d7
Zach Reed <reedz@oregonstate.edu>
2020-07-06 19:51:32 -0500

    uses env variables to connect to psql

--- api/db/index.js
+++ api/db/index.js
@@ -1,12 +1,14 @@
 
 const { Client } = require('pg')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const client = new Client({
-  user: 'user',
-  host: 'psql',
-  database: 'database',
-  password: 'password',
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 })
 
 client.connect(err => {
