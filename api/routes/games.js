4342b5f913534cd1adabd90b68bfecfe96b0d9e6
mobilemelody <melody.reebs@gmail.com>
2020-07-07 22:38:39 -0700

    Added DB connection to add games

--- api/routes/games.js
+++ api/routes/games.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db');
 
 const AWS = require('aws-sdk');
 AWS.config.update({
@@ -9,9 +10,35 @@ AWS.config.update({
 });
 const S3_BUCKET = process.env.Bucket;
 
+/* Get all games */
+router.get('/', (req, res, next) => {
+  db.client.query('SELECT * FROM "Game"', (err, result) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    res.status(200).send(result.rows);
+  });
+});
+
 /* Add a game */
 router.post('/', (req, res, next) => {
-  const fields = ["name", "description", "image", "publisher", "min_players", "max_players", "min_playtime", "max_playtime", "year", "min_age"];
+
+  // Map form field names to database field names
+  const fields = {
+    name: 'name',
+    description: 'description',
+    image: '"imgFileName"',
+    publisher: 'publisher',
+    min_players: '"minPlayers"',
+    max_players: '"maxPlayers"',
+    min_playtime: '"minPlaytime"',
+    max_playtime: '"maxPlaytime"',
+    year: 'year',
+    min_age: '"minAge"',
+  };
+
+  // Create array of field names for query
+  let query_fields = Object.keys(fields).map(e => fields[e]);
 
   // Validate data received
   if (!("name" in req.body)) {
@@ -21,13 +48,26 @@ router.post('/', (req, res, next) => {
 
   // Build query object
   let query = { text: '', values: [] };
-  fields.forEach(field => {
+  for (let field in fields) {
     query.values.push(req.body[field] || null);
-  });
-  query.text = 'INSERT INTO games(' + fields.join(', ') + ') VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)';
+  }
 
-  // TODO: Run query
-  res.status(201).send(query);
+  // TODO: Add user details
+  query_fields.push('"isUserCreated"');
+  query.values.push(true);
+  query_fields.push('"identifierID"');
+  query.values.push("userId");
+
+  query.text = 'INSERT INTO "Game"(' + query_fields.join(', ') + ') VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *';
+
+  // Run query
+  db.client.query(query, (err, result) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    res.status(200).send(result.rows[0]);
+    // TODO: Save categories
+  });
 
 });
 
