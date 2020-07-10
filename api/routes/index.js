301018ca77373ce28cee6606ef62aadc399ddd9e
Jed Piezas <jed.piezas@gmail.com>
2020-07-09 21:49:13 -0700

    Make root route redirect to localhost:8080 if is local dev

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (process.env.NODE_ENV === 'dockerlocal') {
+    return res.redirect('http://localhost:8080');
+  }
   res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
