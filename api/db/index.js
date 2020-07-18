a527cd508dbbd5c5b9c42b4569b291388df4056f
Jed Piezas <jed.piezas@gmail.com>
2020-07-17 17:44:52 -0700

    Change to use DATABASE_URL

--- api/db/index.js
+++ api/db/index.js
@@ -3,11 +3,8 @@ const { Client } = require('pg')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const isProduction = process.env.NODE_ENV === 'production';
-const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-
 const client = new Client({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+  connectionString: process.env.DATABASE_URL,
 })
 
 client.connect(err => {
