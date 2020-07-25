8c96ad61a40aef5689b20dd073fe4c9c3f91546a
Jed Piezas <jed.piezas@gmail.com>
2020-07-25 09:43:44 -0700

    Add user collections route and update Dockerfile

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,8 +4,7 @@ const db = require('../db');
 
 /* GET users listing. */
 router.get('/:id', (req, res, next) => {
-  const getUserDataQuery = 'SELECT * FROM "User"' +
-  ' WHERE "User".id = $1';
+  const getUserDataQuery = 'SELECT * FROM "User" WHERE "User".id = $1';
 
   const query = {
     text: getUserDataQuery,
@@ -27,6 +26,36 @@ router.get('/:id', (req, res, next) => {
     const { password, ...userData } = result.rows[0];
 
     return res.status(200).send(userData);
+  });
+});
+
+router.get('/:id/collections', (req, res, next) => {
+  const getUserCollectionsQuery = 'SELECT DISTINCT ON("UserCollection"."id") "Game"."imgFileName" as coverimg, "UserCollection".*' +
+    ' FROM "UserCollection"' +
+    ' JOIN "CollectionGame" ON "CollectionGame"."userColllectionID" = "UserCollection".id' +
+    ' JOIN "Game" ON "CollectionGame"."gameID" = "Game".id' +
+    ' WHERE "UserCollection"."userID" = $1' +
+    ' ORDER BY "UserCollection".id ASC';
+
+  const query = {
+    text: getUserCollectionsQuery,
+    values: [req.params.id]
+  };
+
+  db.client.query(query, (err, result) => {
+    if (err) {
+      console.error(`Error querying user collections: ${err.message}`);
+
+      return res.status(500).send('Internal server error');
+    }
+
+    const collections = result.rows;
+
+    if (!collections || collections.length < 1) {
+      return res.status(404).send('Not found');
+    }
+
+    return res.status(200).send(collections);
   });
 });
 
