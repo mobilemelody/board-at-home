4a34c1dca48643890d77dd684a9c1dc67954848b
Jed Piezas <jed.piezas@gmail.com>
2020-07-24 17:42:54 -0700

    Add initial get user by id endpoint

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.status(200).send({message: 'suhh dude'});
+router.get('/:id', (req, res, next) => {
+  const getUserDataQuery = 'SELECT * FROM "User"' +
+  ' WHERE "User".id = $1';
+
+  const query = {
+    text: getUserDataQuery,
+    values: [req.params.id]
+  }
+
+  db.client.query(query, (err, result) => {
+    if (err) {
+      console.error(`Error querying user profile: ${err.message}`);
+
+      return res.status(500).send('Internal server error');
+    }
+
+    if (!result.rows[0]) {
+      return res.status(404).send('Not found');
+    }
+
+    // Remove password from userData that we return to UI
+    const { password, ...userData } = result.rows[0];
+
+    return res.status(200).send(userData);
+  });
 });
 
 module.exports = router;
