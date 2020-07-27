a62aadcfd008414c162252e9b01e0e237b9aeef3
Jed <Jedeu@users.noreply.github.com>
2020-07-27 15:33:04 -0700

    Add routes for User Profile page (#22)
    * Add initial get user by id endpoint
    
    * Add user collections route and update Dockerfile
    
    * Remove route users/:id/collections route

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,8 +5,30 @@ const dbUtils = require('../utils/db.js');
 const apiUtils = require('../utils/api.js');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.status(200).send({message: 'suhh dude'});
+router.get('/:id', (req, res, next) => {
+  const getUserDataQuery = 'SELECT * FROM "User" WHERE "User".id = $1';
+
+  const query = {
+    text: getUserDataQuery,
+    values: [req.params.id]
+  }
+
+  db.client.query(query, (err, result) => {
+    if (err) {
+      console.error(`Error querying user profile: ${err.message}`);
+
+      return res.status(500).send('Internal server error');
+    }
+
+    if (!result.rows[0]) {
+      return res.status(404).send('Not found');
+    }
+
+    // Remove password from userData that we return to UI
+    const { password, ...userData } = result.rows[0];
+
+    return res.status(200).send(userData);
+  });
 });
 
 /* Get user collections */
