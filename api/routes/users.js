ef916cc6af0a8bf95eef6583907b9599bf66396f
mobilemelody <melody.reebs@gmail.com>
2020-07-29 21:14:47 -0700

    Remove error if no user collections

--- api/routes/users.js
+++ api/routes/users.js
@@ -52,12 +52,6 @@ router.get('/:user_id/collections', (req, res) => {
   db.client.query(query, (err, result) => {
     if (err) {
       return res.status(400).send(err);
-    } else if (!result.rows.length) {
-      err = { "Error": "This user does not have any collections" };
-      err.Error += "gameID" in req.query ? ' with this game' : '';
-      return res.status(404)
-        .set({ "Content-Type": "application/json" })
-        .send(err);
     }
 
     let response = {};
